d8225371797b168659e8d555d6276be28c507cb0
neox <mnshchtri@gmail.com>
2023-04-09 02:40:21 +0545

    Test commit

--- Password.py
+++ Password.py
@@ -2,11 +2,12 @@
 import secrets
 
 # Declare a list of characters to choose from
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{};:,./<>?`~'
-
+chars_1 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{};:,./<>?`~'
+chars_2 = '0123456789'
 # Use the secrets module to generate a secure random password
-password = ''.join(secrets.choice(chars) for i in range(30))
+password = ''.join(secrets.choice(chars_1 + chars_2) for i in range(30))
 
 # Print the generated password
 print(password)
-print("Boom! you got a secure password")
+print("Boom! you got a  more secure password")
+print("Update your Password in time.....")
